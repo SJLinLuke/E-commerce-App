68e6848975b2d878def7a5b1c93b49a1c0d2fecb
Luke Lin <luke@playmore.com.hk>
2024-05-23 14:38:26 +0800

    [REMOVE] remove useless navStack that cause UI issue

--- CitySuper-Eshop clone/View/Common/ProductVGridView/ProductItem/ProductItem.swift
+++ CitySuper-Eshop clone/View/Common/ProductVGridView/ProductItem/ProductItem.swift
@@ -25,77 +25,76 @@ struct ProductItem: View {
     }
     
     var body: some View {
-        NavigationStack {
-            ZStack {
-                Color(hex: "#F2F2F2")
-                VStack(alignment: .leading) {
-                    RemoteImageView(url: VM.product?.image_src ?? "",
-                                    placeholder: .common)
-                        .frame(height: height / 1.6)
-                    
-                    Spacer()
-                        .frame(width: width - 3, height: 1)
-                        .overlay(alignment: .trailing) {
-                            if (!isNeedDelete) {
-                                FavouriteButton(isFavourite: VM.product?.is_favourite ?? false, width: 30, height: 30)
-                            }
-                        }
-                    
-                    Text(VM.product?.title ?? "")
-                        .multilineTextAlignment(.leading)
-                        .font(.caption)
-                        .fontWeight(.semibold)
-                        .padding(EdgeInsets(top: 15, leading: 10, bottom: 0, trailing: 5))
-                    
-                    Spacer()
-                    
-                    HStack(alignment: .bottom) {
-                        VStack(alignment: .leading) {
-                            if (VM.isCompareWithPrice) {
-                                Text("\(Currency.stringFrom(Decimal(string: VM.product?.compare_at_price ?? "0") ?? 0.0))")
-                                    .font(.caption)
-                                    .strikethrough()
-                                    .foregroundColor(.secondary)
-                            }
-
-                            Text("\(Currency.stringFrom(Decimal(string: VM.product?.price ?? "0") ?? 0.0))")
-                                .font(.caption)
-                                .foregroundColor(VM.isCompareWithPrice ? .red : .black )
-                        }
-
-                        Spacer()
-                        
-                        if VM.isSoldOut {
-                            SoldOutButton(width: 53, height: 25)
-                        } else {
-                            CartButton(width: 25, height: 25)
+        ZStack {
+            Color(hex: "#F2F2F2")
+            VStack(alignment: .leading) {
+                RemoteImageView(url: VM.product?.image_src ?? "",
+                                placeholder: .common)
+                .frame(height: height / 1.6)
+                
+                Spacer()
+                    .frame(width: width - 3, height: 1)
+                    .overlay(alignment: .trailing) {
+                        if (!isNeedDelete) {
+                            FavouriteButton(isFavourite: VM.product?.is_favourite ?? false, width: 30, height: 30)
                         }
                     }
-                    .padding(5)
-                }
-            }
-            .onTapGesture {
-                selectedProductID = VM.product?.shopify_product_id ?? ""
-            }
-            .navigationDestination(item: $selectedProductID, destination: { id in
-                ProductDetailView(shopifyID: id)
-            })
-            .frame(width: width, height: height)
-            .cornerRadius(10)
-            .shadow(color: .gray, radius: 2, x: 0.5, y: 0.5)
-            .padding(.leading, 3)
-            .overlay(alignment:.topTrailing) {
-                if (isNeedDelete) {
-                    ZStack {
-                        Button {
-                            print("tap delete!")
-                        } label: {
-                            Image("favourite_delete_icon")
+                
+                Text(VM.product?.title ?? "")
+                    .multilineTextAlignment(.leading)
+                    .font(.caption)
+                    .fontWeight(.semibold)
+                    .padding(EdgeInsets(top: 15, leading: 10, bottom: 0, trailing: 5))
+                
+                Spacer()
+                
+                HStack(alignment: .bottom) {
+                    VStack(alignment: .leading) {
+                        if (VM.isCompareWithPrice) {
+                            Text("\(Currency.stringFrom(Decimal(string: VM.product?.compare_at_price ?? "0") ?? 0.0))")
+                                .font(.caption)
+                                .strikethrough()
+                                .foregroundColor(.secondary)
                         }
+                        
+                        Text("\(Currency.stringFrom(Decimal(string: VM.product?.price ?? "0") ?? 0.0))")
+                            .font(.caption)
+                            .foregroundColor(VM.isCompareWithPrice ? .red : .black )
+                    }
+                    
+                    Spacer()
+                    
+                    if VM.isSoldOut {
+                        SoldOutButton(width: 53, height: 25)
+                    } else {
+                        CartButton(width: 25, height: 25)
+                    }
+                }
+                .padding(5)
+            }
+        }
+        .onTapGesture {
+            selectedProductID = VM.product?.shopify_product_id ?? ""
+        }
+        .navigationDestination(item: $selectedProductID, destination: { id in
+            ProductDetailView(shopifyID: id)
+        })
+        .frame(width: width, height: height)
+        .cornerRadius(10)
+        .shadow(color: .gray, radius: 2, x: 0.5, y: 0.5)
+        .padding(.leading, 3)
+        .overlay(alignment:.topTrailing) {
+            if (isNeedDelete) {
+                ZStack {
+                    Button {
+                        print("tap delete!")
+                    } label: {
+                        Image("favourite_delete_icon")
                     }
                 }
             }
         }
+        
     }
 }
 
