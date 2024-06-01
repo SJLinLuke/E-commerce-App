edfe59554f3384c1de0f1a2d2082817d24e5f342
Luke <bbc14160015@gmail.com>
2024-06-01 13:08:21 +0800

    [REFACTOR] refactor alertItem & alertContext

--- CitySuper-Eshop clone/Utilities/Alert/AlertContext.swift
+++ CitySuper-Eshop clone/Utilities/Alert/AlertContext.swift
@@ -9,32 +9,43 @@ import SwiftUI
 
 struct AlertItem: Identifiable {
     let id = UUID()
-    let title: Text
-    let message: Text
-    var dismissButton: Alert.Button?
     
-    init(title: Text, message: Text, dismissButton: Alert.Button? = .default(Text("Ok"))) {
+    let title: String
+    let message: Text
+    var buttons: [AlertButton] = []
+    
+    init(title: String, message: Text, buttons: [AlertButton]? = nil) {
         self.title = title
         self.message = message
-        self.dismissButton = dismissButton
+        self.buttons = buttons ?? []
     }
+}
+
+struct AlertButton: Identifiable {
+    let id = UUID()
+    let title: String
+    var role: ButtonRole? = .none
+    let action: () -> Void
 }
 
 struct AlertContext {
     
     //MARK: - Network Alerts
     
-    static let inValidURL       = AlertItem(title: Text("Server Error"),
-                                            message: Text("The data received from the server is unvalid. Please contact support."))
+    static let inValidURL      = AlertItem(title: "Server Error",
+                                           message: Text("The data received from the server is unvalid. Please contact support."))
     
-    static let inValidResponse  = AlertItem(title: Text("Server Error"),
-                                            message: Text("Invalid response from the server. Please try again later or contact support."))
+    static let inValidResponse = AlertItem(title: "Server Error",
+                                           message: Text("Invalid response from the server. Please try again later or contact support."))
     
-    static let inValidData      = AlertItem(title: Text("Server Error"),
-                                            message: Text("There was an issue connecting to the server. If this persists, please contact support."))
+    static let inValidData     = AlertItem(title: "Server Error",
+                                           message: Text("There was an issue connecting to the server. If this persists, please contact support."))
     
     //MARK: - Profile Alerts
     
-    static let deliveryAddress_delete = AlertItem(title: Text("Notice"), 
-                                                  message: Text("Are you sure to delete this saved address?"))
+    static let deliveryAddress_delete   = AlertItem(title: "Notice",
+                                                    message: Text("Are you sure to delete this saved address?"))
+    
+    static let deliveryAddress_fillForm = AlertItem(title: "Notice",
+                                                    message: Text("Please fill in all required fields"))
 }
