df6e7d1d49182b9bfe1b8ecae178a335e4c42a72
Luke <bbc14160015@gmail.com>
2024-06-02 10:34:12 +0800

    [REFACTOR] loadingView improvement

--- CitySuper-Eshop clone/View/Common/LoadingIndicatorView/LoadingIndicatiorView.swift
+++ CitySuper-Eshop clone/View/Common/LoadingIndicatorView/LoadingIndicatiorView.swift
@@ -9,29 +9,36 @@ import SwiftUI
 
 struct LoadingIndicatiorView: View {
     
-    //    var isShowing: Bool
-    
     @State private var degree     : Int = 270
     @State private var spinnerLength = 0.6
     
     var body: some View {
-        ZStack{
+        ZStack {
             Circle()
-                .trim(from: 0.0,to: spinnerLength)
-                .stroke(LinearGradient(colors: [.black,.gray], startPoint: .topLeading, endPoint: .bottomTrailing),style: StrokeStyle(lineWidth: 3.0,lineCap: .round,lineJoin:.round))
-                .animation(Animation.easeIn(duration: 1.5).repeatForever(autoreverses: true))
-                .frame(width: 55,height: 55)
+                .trim(from: 0.0, to: spinnerLength)
+                .stroke(
+                    LinearGradient(colors: [.black, .gray], startPoint: .topLeading, endPoint: .bottomTrailing),
+                    style: StrokeStyle(lineWidth: 1.8, lineCap: .round, lineJoin: .round)
+                )
+                .frame(width: 55, height: 55)
                 .rotationEffect(Angle(degrees: Double(degree)))
-                .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
-                .onAppear{
-                    degree = 270 + 360
-                    spinnerLength = 0
+                .onAppear {
+                    startAnimations()
                 }
         }
         .frame(width: 70, height: 70)
-        .background(Color.commonBackGroundGray)
+        .background(Color.commonBackGroundGray.opacity(0.9))
         .cornerRadius(10)
         .shadow(radius: 40)
+    }
+    
+    private func startAnimations() {
+        withAnimation(Animation.linear(duration: 1).repeatForever(autoreverses: false)) {
+            degree = 270 + 360
+        }
+        withAnimation(Animation.easeIn(duration: 1.5).repeatForever(autoreverses: true)) {
+            spinnerLength = 0
+        }
     }
 }
 
