8eb465df3738c49f66854ec39aaa7032ff817c4e
Luke <bbc14160015@gmail.com>
2024-06-10 22:46:47 +0800

    [REFACTOR] new bullet point view with modes

---
+++ CitySuper-Eshop clone/View/Common/BulletPointsView/BulletPointsView.swift
@@ -0,0 +1,41 @@
+//
+//  ProductDetailBulletPointsView.swift
+//  CitySuper-Eshop clone
+//
+//  Created by LukeLin on 2024/5/21.
+//
+
+import SwiftUI
+
+enum BulletPointMode {
+    case normal
+    case cart
+}
+
+struct BulletPointsView: View {
+        
+    let tags: [LogisticTag]
+    var mode: BulletPointMode = .normal
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            ForEach(tags, id: \.self) { tag in
+                HStack {
+                    let frameSize: CGFloat = mode == .cart ? 15 : 20
+                    Image(tag.typeImage)
+                        .resizable()
+                        .frame(width: frameSize, height: frameSize)
+                    
+                    Text(mode == .cart ? tag.tag_name : tag.bullet_point ?? "")
+                        .font(mode == .cart ? .caption : .subheadline)
+                        .foregroundColor(.gray)
+                }
+                .padding(mode == .cart ? .bottom : .vertical)
+            }
+        }
+    }
+}
+
+#Preview {
+    BulletPointsView(tags: [LogisticTag(bullet_point: "test_bullet_point", tag_name: "test_tag_name", type: "")])
+}
